5e9b6f7f695ae1a1bb87035c5c1fb00dc219448e
ACosmicWolf <cosmicwolf009@gmail.com>
2024-05-31 19:01:18 +0530

    Version 2 of portfolio

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="content">
+	<span>Hey there, I am Arsh!</span>
+</div>
+
+<style>
+	.content {
+		color: #c1cbf4;
+		/* font-size: large; */
+		font-weight: bolder;
+		text-shadow:
+			1px 1px 2px #fff,
+			1px 1px 1px #ccc;
+		color: #fff;
+		text-align: center;
+		font-family: 'lato', sans-serif;
+		font-size: 50px;
+		letter-spacing: 10px;
+		margin-top: -60px;
+		padding-left: 10px;
+
+		/* mkae text size smaller on mobile devices */
+		@media (max-width: 600px) {
+			font-size: 30px;
+			letter-spacing: 5px;
+			padding: 15px;
+		}
+	}
+</style>
